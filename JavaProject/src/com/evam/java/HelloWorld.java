7604451edf3384cd595aa3bff174730a75c5502c
bkirankumar <bkiran.2010@gmail.com>
2012-08-24 18:26:20 +0530

    Update JavaProject/src/com/evam/java/HelloWorld.java
    haiiiiiiiii

--- JavaProject/src/com/evam/java/HelloWorld.java
+++ JavaProject/src/com/evam/java/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 public static void main(String[] args) {
 	System.out.println("******Welcome To Git*******");
 	System.out.println();
-	System.out.println("This is from Harrischandra");)
+	System.out.println("hello");
+	System.out.println("This is from Harrischandra");
 }
 }
